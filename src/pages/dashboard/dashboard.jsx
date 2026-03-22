25fcef221944663156a29d5d843e2e6e2d21626b
Lillyb6 <113935001+Lillyb6@users.noreply.github.com>
2026-03-22 04:05:21 -0500

    Add dashboard navigation buttons

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { auth, db } from '../../config/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
 import DailyQuoteCard from '../../components/DailyQuoteCard';
+import StreakCard from '../../components/StreakCard';
 import './dashboard.css';
 
 const Dashboard = () => {
   const [user, setUser] = useState(undefined);
-  const [streak, setStreak] = useState(0);
+  
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -39,13 +40,13 @@ const Dashboard = () => {
 
       {user ? (
         <>
-        <DailyQuoteCard uid={user.uid} />
 
           <DailyQuoteCard uid={user.uid} />
 
           <div className="bottom-boxes">
             <div className="left-container">
               <StreakCard />
+              
               <button 
                 className="habits-btn"
                 onClick={() => navigate('/tasks')}
@@ -54,19 +55,21 @@ const Dashboard = () => {
               </button>
             </div>
 
-            <div className="right-container">
-              <h2>Your Plants</h2>
-              <button 
-                className="plants-btn"
-                onClick={() => navigate('/plant')}
-              >
-                view your plant
-              </button>
-            </div>
+          <div className="right-container">
+            <h2>Your Plants</h2>
+            <button 
+              className="plants-btn"
+              onClick={() => navigate('/plant')}
+            >
+              view your plants
+            </button>
+
+          </div>
 
           </div>
 
           
+        
         </>
       ) : (
         <p>Please log in to view your daily quote.</p>
